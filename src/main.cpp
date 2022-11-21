45d58b1cb1bae7b58a9eba81b63a32d10ebe6a75
Paul Gangneux <paul.v.gangneux@gmail.com>
2022-11-21 14:34:13 +0100

    dylan is mean :c

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ using namespace sf;
 using namespace std;
 
 int main() {
+  bool mouseIsIn = true;
+  // bool mousePressed = false;
   Win win(800, 600, "Gaming");
 
   // exemple d'affichage de texte (todo: encapsuler dans une classe)
@@ -15,13 +17,29 @@ int main() {
   Text text;
   text.setFont(font);
   text.setString("Gaming");
-  text.setCharacterSize(100);
+  text.setCharacterSize(30);
   text.setFillColor(Color::White);
   text.setPosition(0, 0);
 
+  float tx = 0, ty = 0;
+
   bool runs = true;
 
+  float dx = 0;
+  float dy = 0;
+
+  Vector2i mousePos = Mouse::getPosition(win);
+
+  float mx = mousePos.x;
+  float my = mousePos.y;
+
   while (runs) {
+
+    dx = mousePos.x - mx;
+    mx = mousePos.x;
+    dy = mousePos.y - my;
+    my = mousePos.y;
+    mousePos = Mouse::getPosition(win);
 
     Event event;
     while (win.pollEvent(event)) {
@@ -29,10 +47,28 @@ int main() {
         case Event::Closed:
           runs = false;
           break;
+        case Event::MouseLeft:
+          mouseIsIn = false;
+          break;
+        case Event::MouseEntered:
+          mouseIsIn = true;
+          break;
         default:
           break;
       }
     }
+
+    if (win.hasFocus() && mouseIsIn) {
+      if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+        tx += dx;
+        ty += dy;
+        text.move(dx, dy);
+        text.setScale(sf::Vector2f(1,1));
+      }
+    }
+
+    text.setString(to_string(tx) + " " + to_string(ty));
+
     win.clear();
     win.draw(text);
     win.display();
